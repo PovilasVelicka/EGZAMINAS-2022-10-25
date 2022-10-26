77e79d1c53f05da1212c8f31dc8fc5ac5c214acb
Povilas Velicka <velicka.povilas@gmail.com>
2022-10-26 14:02:51 +0300

    account service created

---
+++ RegistrationSystem.BusinessLogic/Services/AccountServices/AccountService.cs
@@ -0,0 +1,30 @@
+﻿using RegistrationSystem.BusinessLogic.DTOs;
+using RegistrationSystem.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationSystem.BusinessLogic.Services.AccountServices
+{
+    internal class AccountService : IAccountService
+    {
+        private readonly IAccountRepository _repository;
+
+        public Task<IServiceResponseDto<Account>> GetUserInfoAsync (Guid accountId)
+        {
+            throw new NotImplementedException( );
+        }
+
+        public Task<IServiceResponseDto<string>> RemoveAccountAsync (int userId)
+        {
+            throw new NotImplementedException( );
+        }
+
+        public Task<IServiceResponseDto<Account>> UpdateUserInfoAsync (Guid accountId, UserInfo account)
+        {
+            throw new NotImplementedException( );
+        }
+    }
+}
---
+++ RegistrationSystem.BusinessLogic/Services/AccountServices/IAccountService.cs
@@ -0,0 +1,18 @@
+﻿using RegistrationSystem.BusinessLogic.DTOs;
+using RegistrationSystem.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationSystem.BusinessLogic.Services.AccountServices
+{
+    public interface IAccountService
+    {
+        Task<IServiceResponseDto<Account>> GetUserInfoAsync (Guid accountId);
+        Task<IServiceResponseDto<string>> RemoveAccountAsync (int userId);
+        Task<IServiceResponseDto<Account>> UpdateUserInfoAsync (Guid accountId, UserInfo account);
+
+    }
+}
