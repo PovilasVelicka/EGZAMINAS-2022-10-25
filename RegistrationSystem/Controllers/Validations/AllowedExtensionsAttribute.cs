7445468f6952574eb508f2ab11dd3df3bf802137
Povilas Velicka <velicka.povilas@gmail.com>
2022-10-28 08:50:30 +0300

    add extention validation

--- RegistrationSystem/Controllers/Validations/AllowedExtensionsAttribute.cs
+++ RegistrationSystem/Controllers/Validations/AllowedExtensionsAttribute.cs
@@ -4,5 +4,24 @@ namespace RegistrationSystem.Controllers.Validations
 {
     public class AllowedExtensionsAttribute : ValidationAttribute
     {
+        private readonly string[ ] _allowedExtensions;
+        public AllowedExtensionsAttribute (string[ ] extensions)
+        {
+            _allowedExtensions = extensions;
+        }
+
+        protected override ValidationResult? IsValid (object? value, ValidationContext validationContext)
+        {
+            if (value is IFormFile file)
+            {
+                var extention = Path.GetExtension(file.FileName);
+                if (!_allowedExtensions.Contains(extention.ToLower( )))
+                {
+                    return new ValidationResult($"Foto type is not supported, supproted types: {string.Join("; ", _allowedExtensions)}");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
     }
 }
